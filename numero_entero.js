39e7d7343ff54fcbdf048b6b2eebdd68c70d1d4a
Erick Martinez <erick_martinez9101@outlook.com>
2021-11-19 13:04:05 -0600

    éste programa devuelve la parte entera de el número que sea ingresado

---
+++ numero_entero.js
@@ -0,0 +1,38 @@
+function getFloor(numero) {
+
+
+    return Math.floor(numero);
+ }
+
+
+
+/**El método Math.Floor devuelve el máximo entero redondeado más bajo de la variable
+ * en el caso de los negativos, devueve el siguiente (-44.5) devolvería el -45 ya que
+ * matemáticamente hablando es más chico que el (-44.5).
+ */
+
+
+
+/**El método Math.trunc devuelve la parte entrea de cualquier número quitando la parte decimal 
+ * sin importar si el argumento es un número positivo o negativo. Es decir no pasa como en 
+ * Math.floor que redondea al más pequeño sino en éste caso quita los valores despues del punto
+ * y deja el valor entero sin cambios.
+ */
+
+
+
+function getTrunc(numero) {
+
+    return Math.trunc(numero);
+    
+}
+
+
+
+
+
+
+
+
+
+
